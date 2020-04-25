caf4cd0fa59704868b248134566d9aee53d0fac5
hdpuk86 <hayley.d.prior@gmail.com>
2020-04-25 17:33:42 +0100

    add user controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  def show
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:display_name)
+    end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+def setup
+    @user = users(:one)
+  end
+
+  def test_should_get_index
+    get users_url
+    assert_response :success
+  end
+
+  def test_should_show_user
+    get user_url(@user)
+    assert_response :success
+  end
+
+  def test_should_get_edit
+    get edit_user_url(@user)
+    assert_response :success
+  end
+
+  def test_should_update_user
+    patch user_url(@user), params: { user: {
+      display_name: 'name'
+    } }
+    assert_redirected_to user_url(@user)
+  end
+end
