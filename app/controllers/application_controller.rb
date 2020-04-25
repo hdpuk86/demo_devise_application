db31f8c96a9e89aab360259e8ae9ea01b527ba0b
hdpuk86 <hayley.d.prior@gmail.com>
2020-04-25 20:23:38 +0100

    protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
 
   def after_sign_out_path_for(_resource_or_scope)
